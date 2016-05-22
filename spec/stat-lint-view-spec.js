264bf523ebe759af790d413caf5ee9034c62e3f1
jgkiliris <giannis9gk@gmail.com>
2016-05-23 00:26:50 +0300

    First Commit

---
+++ spec/stat-lint-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StatLintView from '../lib/stat-lint-view';
+
+describe('StatLintView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
